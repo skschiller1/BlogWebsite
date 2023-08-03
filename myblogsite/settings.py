d60fc2926195318d40b81b8ee82c86d0ad5d65e5
skschiller1 <112667643+skschiller1@users.noreply.github.com>
2023-08-03 16:19:17 -0500

    Adding secret key functionality

--- myblogsite/settings.py
+++ myblogsite/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = 'django-insecure-hap1$x(^$&n(e#-q^b#3i(x^sad!q4^bgy5o!&_l8=*@xq-vbu'
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
--- myblogsite/wsgi.py
+++ myblogsite/wsgi.py
@@ -8,9 +8,12 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
+from dotenv import load_dotenv
 
+
+project_folder = os.path.expanduser('~/BlogWebsite')
+load_dotenv(os.path.join(project_folder, '.env'))
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myblogsite.settings')
 
 application = get_wsgi_application()
