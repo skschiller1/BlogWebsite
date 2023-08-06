60325a67eb06b1e6f5f97543882b89f4a2304dfe
skschiller1 <112667643+skschiller1@users.noreply.github.com>
2023-08-06 15:47:23 -0500

    Chaning mediafile upload path

--- myblogsite/settings.py
+++ myblogsite/settings.py
@@ -110,11 +110,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'myblogsite/static')
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, "static/"),]
 
 MEDIA_URL = 'media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'myblogsite/media')
 # MEDIAFILES_DIRS = [os.path.join(BASE_DIR, 'media'),]
 
 # Default primary key field type
