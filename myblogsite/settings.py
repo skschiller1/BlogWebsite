c2f0a92854c091dba90f34e5990060b2fcac3cb4
skschiller1 <112667643+skschiller1@users.noreply.github.com>
2023-08-26 01:07:57 -0500

    Uploading same fuel program update but with changed secret_key and secret_key hidden

--- myblogsite/settings.py
+++ myblogsite/settings.py
@@ -9,12 +9,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = os.getenv('SECRET_KEY')
-SECRET_KEY = '^j&-u^aaxjc$6^c69_f+=t4lh%f43t*%$sej3)kx__s!e8z1i('
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '.pythonanywhere.com',
