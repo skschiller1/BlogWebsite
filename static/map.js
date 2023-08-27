c5a682791ef396f65f808e69c27d4598585ce85e
skschiller1 <112667643+skschiller1@users.noreply.github.com>
2023-08-26 23:40:25 -0500

    string

--- static/map.js
+++ static/map.js
@@ -29,7 +29,7 @@ if (fuel_airports.length > 1) {
         alert("Inside for loop");
         alert(fuel_airports[i].innerHTML);
         var namecalllatlong = fuel_airports[i].innerHTML.split();
-        alert(namecalllatlong[2], namecalllatlong[3]);  // The problem is on this line atm.
+        alert(namecalllatlong);  // The problem is on this line atm.
         let marker = L.marker([parseFloat(namecalllatlong[2]), parseFloat(namecalllatlong[3])]);
 
         list_of_markers.push(marker);
