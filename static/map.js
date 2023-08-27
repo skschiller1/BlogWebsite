3296cd393b3b34c3642d839d21eba3d192c9ce16
skschiller1 <112667643+skschiller1@users.noreply.github.com>
2023-08-27 13:34:21 -0500

    markers

--- static/map.js
+++ static/map.js
@@ -2,8 +2,8 @@ function MarkerFunction(x, map, color) {
     var myIcon = L.icon({
         iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
         shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
-        iconSize: [25, 30],
-        iconAnchor: [22, 94],
+        iconSize: [25, 40],
+        iconAnchor: [0, 40],
         popupAnchor: [-3, -76],
         shadowSize: [68, 95],
         shadowAnchor: [22, 94]
