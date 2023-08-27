1b5559ad8dae728bb3dc5e4bef07c45dedc628f2
skschiller1 <112667643+skschiller1@users.noreply.github.com>
2023-08-27 13:56:00 -0500

    markers

--- static/map.js
+++ static/map.js
@@ -6,7 +6,7 @@ function MarkerFunction(x, map, color) {
         iconAnchor: [13, 41],
         popupAnchor: [0, -34],
         shadowSize: [40, 53],
-        shadowAnchor: [12, 55]
+        shadowAnchor: [12, 75]
     });
 
     const PI = Math.PI;
