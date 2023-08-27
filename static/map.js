41c9716e86e17408ec36eaed0c9435c17577e77d
skschiller1 <112667643+skschiller1@users.noreply.github.com>
2023-08-27 13:39:22 -0500

    markers

--- static/map.js
+++ static/map.js
@@ -3,10 +3,10 @@ function MarkerFunction(x, map, color) {
         iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
         shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
         iconSize: [25, 40],
-        iconAnchor: [15, 41],
-        popupAnchor: [12, -30],
+        iconAnchor: [12, 41],
+        popupAnchor: [0, -30],
         shadowSize: [68, 95],
-        shadowAnchor: [15, 39]
+        shadowAnchor: [0, 60]
     });
 
     const PI = Math.PI;
