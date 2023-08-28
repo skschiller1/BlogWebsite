958324b8110b75a012b64b93d5078ecafc2bcb5f
skschiller1 <112667643+skschiller1@users.noreply.github.com>
2023-08-27 21:02:57 -0500

    changing url settings

--- blog/urls.py
+++ blog/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 
@@ -6,4 +8,4 @@ urlpatterns = [
     path('', views.blog_index, name='blog_index'),
     path('<int:pk>/', views.blog_detail, name='blog_detail'),
     path('<category>/', views.blog_category, name='blog_category'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
